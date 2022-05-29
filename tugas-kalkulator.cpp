30691f4aa5abc7908263efd0ae247ca9813732bb
reyhanihza <70962894+reyhanihza@users.noreply.github.com>
2022-05-29 22:43:30 +0700

    Menambahkan fungsi trigonometri

--- tugas-kalkulator.cpp
+++ tugas-kalkulator.cpp
@@ -90,6 +90,68 @@ double modulo(){
 	cout <<"Calculation result: " << rounded_first << " mod " << rounded_second << " = " << calculation_result;
 
 }
+
+// Trigonometry
+double Sinus(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = sin(number);
+
+  cout << "sin " << number << " = " << calculation_result << endl;
+}
+
+double Cosinus(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = cos(number);
+
+  cout << "cos " << number << " = " << calculation_result << endl;
+}
+
+double Tangen(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = tan(number);
+
+  cout << "tan " << number << " = " << calculation_result << endl;
+}
+
+double InversSinus(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = asin(number);
+
+  if (number < -1 && number > 1){
+		throw runtime_error("Math error: Domain [-1,1] only\n");
+	}
+  
+  cout << "arcsin " << number << " = " << calculation_result << endl;;
+}
+
+double InversCosinus(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = acos(number);
+  
+  if (number < -1 && number > 1){
+		throw runtime_error("Math error: Domain [-1,1] only\n");
+	}
+
+  cout << "arccos " << number << " = " << calculation_result << endl;;
+}
+
+double InversTangen(){
+  double number{input_number()};
+  double calculation_result;
+  calculation_result = atan(number);
+  
+  if (number < -1 && number > 1){
+		throw runtime_error("Math error: Domain [-1,1] only\n");
+	}
+
+  cout << "arctan " << number << " = " << calculation_result << endl;;
+}
+
 int main(){
 	char continue_key;
 	int option;
@@ -100,6 +162,12 @@ int main(){
 	cout << "3. Perkalian" << endl;
 	cout << "4. Pembagian" << endl;
 	cout << "5. Modulus" << endl;
+	cout << "6.  Sin" << endl;
+	cout << "7.  Cos" << endl;
+	cout << "8.  Tan" << endl;
+	cout << "9.  Arcsin" << endl;
+	cout << "10. Arccos" << endl;
+	cout << "11. Arctan" << endl;
 	cout << endl;
 	
 	START:
@@ -135,13 +203,43 @@ int main(){
 		
 			modulo();
 			break;
+		
+	      	case 6 :
+
+			Sinus();
+			break;
+
+		case 7 :
+
+			Cosinus();
+			break;
+
+		case 8 :
+
+			Tangen();
+			break;
+
+		case 9 :
+
+			InversSinus();
+			break;
+
+		case 10 :
+
+			InversCosinus();
+			break;
+
+		case 11 :
+
+			InversTangen();
+			break;
 			
 		default:
 			
 			cout << endl;
 			cout<<"Selector only accept number (1-9)"<<endl;
 			
-			cout << "Apakah ingin memulai lagi? (Y/N)";
+			cout << "Apakah ingin memulai lagi? (Y/N): ";
 			
 			cin >> continue_key;
 			
